55006632112ee4fa19803be6449ed31ff2a241fd
박영하 (Youngha Park) <youngha0930@outlook.com>
2024-11-13 23:33:01 +0900

    [C1] 02-1

---
+++ programming_beginner/c_programming_1/02/01/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("this is comment1\n");
+    printf("this is comment2\n");
+    printf("this is comment3\n");
+    printf("this is comment4\n");
+    // printf("this is comment5\n");
+    printf("this is comment6\n");
+    /*
+        printf("this is comment7\n");
+        printf("this is comment8\n");
+    */
+    printf("this is comment9\n");
+    return 0;
+}
