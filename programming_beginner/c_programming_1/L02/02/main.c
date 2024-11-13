0bb5306e263e9d00cc39e01e33b9f5ae949a8380
박영하 (Youngha Park) <youngha0930@outlook.com>
2024-11-13 23:49:34 +0900

    [C1] 02-2

---
+++ programming_beginner/c_programming_1/L02/02/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%d,\t%x\n", 16, 16);
+    printf("%g,\t%g\n", 3.01, 1000000.3412567);
+    printf("%c", 'C');
+    printf("%s\n", " Language");
+    return 0;
+}
