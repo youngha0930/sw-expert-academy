0c7793851cddec96f3e9e862c87b6ec23242c12c
박영하 (Youngha Park) <youngha0930@outlook.com>
2024-11-14 00:12:11 +0900

    [C1] 02-5

---
+++ programming_beginner/c_programming_1/L02/05/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    char letter;
+    scanf("%c", &letter);
+    printf("%c\n", letter);
+    printf("%d\n", letter);
+    printf("%x\n", letter);
+    return 0;
+}
